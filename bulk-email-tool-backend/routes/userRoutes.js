c23706f83677c227c6c5e71b70444710f95195ed
nanthakumars156 <nanthakumars156@gmail.com>
2025-01-16 01:56:20 +0530

    c

--- bulk-email-tool-backend/routes/userRoutes.js
+++ bulk-email-tool-backend/routes/userRoutes.js
@@ -32,4 +32,13 @@ router.post("/login", async (req, res) => {
   res.status(200).json({ token });
 });
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
